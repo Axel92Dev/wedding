040d2105246cd8fac8dc096f1ea0598ddb546e4a
Alessandro Romano <alessandro.romano1992@gmail.com>
2020-07-28 23:01:50 +0200

    expose inviti

---
+++ src/pages/invito-party.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import invito_wedding_party from '../images/invito_party.png'
+
+const SecondPage = () => (
+  <Layout>
+    <img src={invito_wedding_party}></img>
+  </Layout>
+)
+
+export default SecondPage
---
+++ src/pages/invito.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import invito from '../images/invito.png'
+
+const SecondPage = () => (
+  <Layout>
+    <img src={invito}></img>
+  </Layout>
+)
+
+export default SecondPage
