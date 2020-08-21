ee32c99bde4eb1045ad69aea487065531e43d219
Alessandro Romano <alessandro.romano1992@gmail.com>
2020-08-21 17:34:51 +0200

    adds social hashtag links

--- src/components/sections.js
+++ src/components/sections.js
@@ -8,10 +8,32 @@ export const sections = [
     id: 'cerimonia',
     title: 'LOCATION',
     image: villa,
-    body: (<>
-      <p><a href="http://www.villailgeraneo.com/" target="_blank" rel="noopener noreferrer">Villa Il Geraneo</a> &egrave; una location suggestiva, incorniciata dal Lago Maggiore, a Monvalle in provincia di Varese. </p>
-      <p className="center">🚘 <a href="https://goo.gl/maps/yoXr6cs7R9CZmsA38" target="_blank" rel="noopener noreferrer">Portami alla Villa!</a> 🚘</p>
-    </>),
+    body: (
+      <>
+        <p>
+          <a
+            href="http://www.villailgeraneo.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Villa Il Geraneo
+          </a>{' '}
+          &egrave; una location suggestiva, incorniciata dal Lago Maggiore, a
+          Monvalle in provincia di Varese.{' '}
+        </p>
+        <p className="center">
+          🚘{' '}
+          <a
+            href="https://goo.gl/maps/yoXr6cs7R9CZmsA38"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Portami alla Villa!
+          </a>{' '}
+          🚘
+        </p>
+      </>
+    ),
   },
   {
     id: 'party',
@@ -21,28 +43,30 @@ export const sections = [
       <>
         <ul id="agenda">
           <li>
-             <span>11:00</span> Accoglienza
+            <span>11:00</span> Accoglienza
           </li>
           <li>
-             <span>11:30</span> Cerimonia
+            <span>11:30</span> Cerimonia
           </li>
           <li>
-             <span>12:00</span> Ricevimento
+            <span>12:00</span> Ricevimento
           </li>
           <li>
-             <span>17:00</span> Wedding Cake <br />
-             🎉<span className="hidden">20:000</span>Inizio Wedding Party
+            <span>17:00</span> Wedding Cake <br />
+            🎉<span className="hidden">20:000</span>Inizio Wedding Party
           </li>
           <li>
-             <span>18:00</span> Pool Party
+            <span>18:00</span> Pool Party
           </li>
           <li>
-             <span>20:00</span> Stop swim, start drink!<br />
-             🍖<span className="hidden">20:000</span>Buffet di Grigliata e Fritti<br />
-             🍹<span className="hidden">20:000</span>Open Bar All Night
+            <span>20:00</span> Stop swim, start drink!
+            <br />
+            🍖<span className="hidden">20:000</span>Buffet di Grigliata e Fritti
+            <br />
+            🍹<span className="hidden">20:000</span>Open Bar All Night
           </li>
           <li>
-             <span>21:00</span> PARTY HARD!
+            <span>21:00</span> PARTY HARD!
           </li>
         </ul>
         <p>Portate costume, ciabatte ed asciugamano!</p>
@@ -56,62 +80,132 @@ export const sections = [
     body: (
       <>
         <p>Bora Bora è lontana... Ma se volete aiutarci a raggiungerla:</p>
-        <p>IBAN: <span className="easy-copy">IT15H0301503200000005625049</span></p>
-        <p>Intestato a: <span className="easy-copy">Alessandro Romano</span></p>
+        <p>
+          IBAN: <span className="easy-copy">IT15H0301503200000005625049</span>
+        </p>
+        <p>
+          Intestato a: <span className="easy-copy">Alessandro Romano</span>
+        </p>
       </>
     ),
   },
   {
     id: 'contatti',
     title: 'CONTATTI',
-    body: <>
-      <p>Giulia</p>
-      <ul className="icons">
-        <li>
-          <a href={`https://wa.me/0393463075899`} className="icon fa-whatsapp" target="_blank">
-            <span className="label">Whatsapp</span>
-          </a>
-        </li>
-        <li>
-          <a href={`tel:+393463075899`} className="icon fa-phone" target="_blank">
-            <span className="label">Telefono</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/giuly732" className="icon fa-facebook" target="_blank">
-            <span className="label">Facebook</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/giuly732" className="icon fa-instagram" target="_blank">
-            <span className="label">Instagram</span>
-          </a>
-        </li>
-      </ul>
-      <p>Alessandro</p>
-      <ul className="icons">
-        <li>
-          <a href={`https://wa.me/0393478858916`} className="icon fa-whatsapp" target="_blank">
-            <span className="label">Whatsapp</span>
-          </a>
-        </li>
-        <li>
-          <a href={`tel:+393478858916`} className="icon fa-phone" target="_blank">
-            <span className="label">Telefono</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/axel92dev"
-            className="icon fa-twitter"
-            target="_blank"
-          >
-            <span className="label">Twitter</span>
-          </a>
-        </li>
-      </ul>
-      <p>È gradita risposta entro il 15 Agosto  🙏</p>
-    </>,
+    body: (
+      <>
+        <p>Giulia</p>
+        <ul className="icons">
+          <li>
+            <a
+              href={`https://wa.me/0393463075899`}
+              className="icon fa-whatsapp"
+              target="_blank"
+            >
+              <span className="label">Whatsapp</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href={`tel:+393463075899`}
+              className="icon fa-phone"
+              target="_blank"
+            >
+              <span className="label">Telefono</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/giuly732"
+              className="icon fa-facebook"
+              target="_blank"
+            >
+              <span className="label">Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/giuly732"
+              className="icon fa-instagram"
+              target="_blank"
+            >
+              <span className="label">Instagram</span>
+            </a>
+          </li>
+        </ul>
+        <p>Alessandro</p>
+        <ul className="icons">
+          <li>
+            <a
+              href={`https://wa.me/0393478858916`}
+              className="icon fa-whatsapp"
+              target="_blank"
+            >
+              <span className="label">Whatsapp</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href={`tel:+393478858916`}
+              className="icon fa-phone"
+              target="_blank"
+            >
+              <span className="label">Telefono</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/axel92dev"
+              className="icon fa-twitter"
+              target="_blank"
+            >
+              <span className="label">Twitter</span>
+            </a>
+          </li>
+        </ul>
+        <p>È gradita risposta entro il 15 Agosto 🙏</p>
+      </>
+    ),
   },
-
+  {
+    id: 'instagram',
+    title: '#alegiulia2020',
+    body: (
+      <>
+        <p>
+          Condividete le vostre foto su tutti i social usando l'hashtag{' '}
+          <strong>#alegiulia2020</strong>
+        </p>
+        <ul className="icons" style={{ textAlign: 'center' }}>
+          <li>
+            <a
+              href="https://www.facebook.com/hashtag/alegiulia2020"
+              className="icon fa-facebook"
+              target="_blank"
+            >
+              <span className="label">Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/explore/tags/alegiulia2020/"
+              className="icon fa-instagram"
+              target="_blank"
+            >
+              <span className="label">Instagram</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/search?q=%23alegiulia2020"
+              className="icon fa-twitter"
+              target="_blank"
+            >
+              <span className="label">Twitter</span>
+            </a>
+          </li>
+        </ul>
+      </>
+    ),
+  },
 ]
